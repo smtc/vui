91c9b52bc1f27df146736dc848e749d22e835450
lobos <lobos841@gmail.com>
2014-10-08 11:11:01 +0800

    ie etag 有问题，继续使用localstorage

--- vui.js
+++ vui.js
@@ -4961,20 +4961,18 @@ var reduce = require('reduce');
 var Authentication = "Authentication",
     token = null;
 
-// 获取token,localStorage 不安全
+// 获取token
 function getToken() {
-    /*
 	token = token === null 
 			? localStorage.getItem(Authentication)
 			: token
-    */
 	return token
 }
 
 function setToken(t) {
     if (!t) return
 	token = t
-	//localStorage.setItem(Authentication, t)
+	localStorage.setItem(Authentication, t)
 }
 
 /**
